f5863739cfe4eb08063950cf18add167ae57412d
Todd Wozny <todd.wozny@myabsc.com>
2014-12-25 21:07:00 -0500

    fixing tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225134953) do
+ActiveRecord::Schema.define(version: 20141226020151) do
 
   create_table "hpins", force: true do |t|
     t.string   "description"
