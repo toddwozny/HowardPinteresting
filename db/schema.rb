c56aeed0a539bc75e6d42d4cfee089a5c409348d
Todd Wozny <todd.wozny@myabsc.com>
2014-12-25 23:46:48 -0500

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -31,16 +31,6 @@ ActiveRecord::Schema.define(version: 20141226020151) do
     t.datetime "updated_at"
   end
 
-  create_table "pins", force: true do |t|
-    t.string   "description"
-    t.string   "string"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
-  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
